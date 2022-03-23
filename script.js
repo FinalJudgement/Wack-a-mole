29db2795b9f80743f749dc2d3c05a10dbb8b119c
Carlos <Silvacj90@gmail.com>
2022-03-23 14:04:50 -0400

    beta

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 const gameOverWindow = document.querySelector(".game-over-window");
 const gameOver = document.querySelector(".game-over");
-
 const playAgain = document.querySelector("#reset-button");
 
 let result = 0;
 let hitPosition;
 let currentTime = 30;
 let timerId = null;
+let countDownTimerId;
+
+function startGame() {
+  grid.style.filter = "blur(5px)";
+  gameOver.innerHTML = "Ready?";
+  playAgain.textContent = "Begin!";
+  gameOverWindow.style.display = "flex";
+}
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -19,10 +26,15 @@ function randomSquare() {
   });
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
-  randomSquare.classList.add("mole");
+
+  setTimeout(() => {
+    randomSquare.classList.add("mole");
+  }, Math.floor(((Math.random() * 10) / 1 + 2) * 100));
+  //   randomSquare.classList.add("mole");
 
   hitPosition = randomSquare.id;
 }
+
 squares.forEach((square) => {
   square.addEventListener("mousedown", () => {
     if (square.id === hitPosition) {
@@ -32,20 +44,21 @@ squares.forEach((square) => {
     }
   });
 });
+
 function moveMole() {
-  timerId = setInterval(randomSquare, 500);
+  timerId = setInterval(randomSquare, 1200);
 }
-
-moveMole();
-
+console.log(Math.floor(((Math.random() * 10) / 2 + 1) * 100));
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
   if (currentTime === 0) {
+    currentTime = 0;
     clearInterval(countDownTimerId);
     clearInterval(timerId);
     grid.style.filter = "blur(5px)";
     gameOver.innerHTML = "Game Over! <br/> Your Final Score is " + result;
+    playAgain.textContent = "Play Again!";
     gameOverWindow.style.display = "flex";
   }
 }
@@ -55,12 +68,11 @@ function resetGame() {
   score.textContent = result;
   currentTime = 30;
   timeLeft.textContent = currentTime;
-  timerId = setInterval(randomSquare, 500);
+  moveMole();
   countDownTimerId = setInterval(countDown, 1000);
   gameOverWindow.style.display = "none";
   grid.style.filter = "none";
 }
 
-let countDownTimerId = setInterval(countDown, 1000);
-
+startGame();
 playAgain.addEventListener("click", resetGame);
